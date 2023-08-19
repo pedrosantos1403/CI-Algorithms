d64855ec4ca6d6250c9d52d3960f00ac9b4c0068
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-08-19 20:49:30 -0300

    plots

--- Fuzzy TSK/tsk.py
+++ Fuzzy TSK/tsk.py
@@ -1,12 +1,14 @@
 import math
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 # define x universe
 ITERATION_LIMIT = 1000
-BEGIN = -200
-END = 200
+BEGIN = -2
+END = 2
 LEARNING_RATE = 0.01
-STEP = 1
+STEP = 0.01
 
 def gaussian_calculator(x, average_x, standart_deviation_x):
     return math.exp((-1/2) * ((x - average_x) / standart_deviation_x)**2)
@@ -37,91 +39,112 @@ def adjust_standart_deviation_x(error, w1, w2, y1, y2, average_x, standart_devia
     else:
         return LEARNING_RATE * (error * w1 * ((y2 - y1) / (w1 + w2)**2) * w2 * (x - average_x)**2 / standart_deviation_x**3)
 
-# define start average_x
-average_x1 = -200
-average_x2 = 200
+def plot(xs, ys, i, average_error):
+    # Criando um gráfico de dispersão
+    plt.scatter(xs, np.power(xs, 2), label='f(x) = x²', color='blue')
+    plt.scatter(xs, ys, label='Fuzzy TSK', color='red')
 
-# define start standart_devitation_x
-standart_deviation_x1 = 150
-standart_deviation_x2 = 150
+    # Adicionando rótulos aos eixos e título ao gráfico
+    plt.xlabel('x')
+    plt.ylabel('y')
 
-# start p1, p2, q1, q2
-p1 = random.random()*abs(BEGIN)
-p2 = random.random()*abs(BEGIN)
-q1 = random.random()*abs(BEGIN)
-q2 = random.random()*abs(BEGIN)
+    plt.title('adjust number: {}\n average error: {}'.format(i,average_error))
+    plt.legend()
+    plt.show()
 
-# start xs and ys
-xs = [x for x in range(BEGIN, END, STEP)]
-ys = [0 for y in range(BEGIN, END, STEP)]
+def main():
+    # define start average_x
+    average_x1 = -2
+    average_x2 = 2
 
-# start errors per point
-errors = [0 for y in range(BEGIN, END, STEP)]
+    # define start standart_devitation_x
+    standart_deviation_x1 = 1.5
+    standart_deviation_x2 = 1.5
 
-for i in range(ITERATION_LIMIT):
-    # pass through xs in a random order
-    indexes = [z for z in range(0,2*END,1)]
-    random.shuffle(indexes)
+    # start p1, p2, q1, q2
+    p1 = random.random()*abs(BEGIN)
+    p2 = random.random()*abs(BEGIN)
+    q1 = random.random()*abs(BEGIN)
+    q2 = random.random()*abs(BEGIN)
 
-    while(indexes != []):
-        # break when there's no more index
-        random_index = indexes.pop()
-        x = xs[random_index]
+    # start xs, ys and errors
+    x = BEGIN
+    xs = []
+    ys = []
+    errors = []
+    while (x <= END):
+        xs.append(x)
+        x += STEP
+        ys.append(0)
+        errors.append(0)
 
-        # calculate gaussian w1
-        w1 = gaussian_calculator(x, average_x1, standart_deviation_x1)
+    for i in range(ITERATION_LIMIT):
+        # pass through xs in a random order
+        indexes = [z for z in range(0,len(xs),1)]
+        random.shuffle(indexes)
 
-        # calculate gaussian w2
-        w2 = gaussian_calculator(x, average_x2, standart_deviation_x2)
+        while(indexes != []):
+            # break when there's no more index
+            random_index = indexes.pop()
+            x = xs[random_index]
 
-        # calculate y1
-        y1 = y_calculator(x, p1, q1)
+            # calculate gaussian w1
+            w1 = gaussian_calculator(x, average_x1, standart_deviation_x1)
 
-        # calculate y2
-        y2 = y_calculator(x, p2, q2)
+            # calculate gaussian w2
+            w2 = gaussian_calculator(x, average_x2, standart_deviation_x2)
 
-        # calculate ys[random_index]
-        ys[random_index] = tsk_calculator(w1,y1,w2,y2)
+            # calculate y1
+            y1 = y_calculator(x, p1, q1)
 
-        # calculate errors[random_index]
-        errors[random_index] = ys[random_index] - x**2
+            # calculate y2
+            y2 = y_calculator(x, p2, q2)
 
-        # update p1
-        p1 -= adjust_pq(errors[random_index], w1, w2, x, 1)
+            # calculate ys[random_index]
+            ys[random_index] = tsk_calculator(w1,y1,w2,y2)
 
-        # update p2
-        p2 -= adjust_pq(errors[random_index], w1, w2, x, 2)
+            # calculate errors[random_index]
+            errors[random_index] = ys[random_index] - x**2
 
-        # update q1
-        q1 -= adjust_pq(errors[random_index], w1, w2, x, 1)
+            # update p1
+            p1 -= adjust_pq(errors[random_index], w1, w2, x, 1)
+
+            # update p2
+            p2 -= adjust_pq(errors[random_index], w1, w2, x, 2)
+
+            # update q1
+            q1 -= adjust_pq(errors[random_index], w1, w2, x, 1)
+            
+            # update q2
+            q2 -= adjust_pq(errors[random_index], w1, w2, x, 2)
+
+            # hold the old average value for future
+            leg_average_x1 = average_x1
+            leg_average_x2 = average_x2
+
+            # update average_x1
+            average_x1 -= adjust_average_x(errors[random_index], w1, w2, y1, y2, average_x1, standart_deviation_x1, x, 1)
+
+            # update average_x2
+            average_x2 -= adjust_average_x(errors[random_index], w1, w2, y1, y2, average_x1, standart_deviation_x1, x, 2)
+            
+            # update standart_deviation_x1
+            # remember: must use old average value
+            standart_deviation_x1 -= adjust_standart_deviation_x(errors[random_index], w1, w2, y1, y2, leg_average_x1, standart_deviation_x1, x, 1)
         
-        # update q2
-        q2 -= adjust_pq(errors[random_index], w1, w2, x, 2)
+            # update standart_deviation_x2
+            # remember: must use old average value
+            standart_deviation_x2 -= adjust_standart_deviation_x(errors[random_index], w1, w2, y1, y2, leg_average_x2, standart_deviation_x2, x, 2)
 
-        # hold the old average value for future
-        leg_average_x1 = average_x1
-        leg_average_x2 = average_x2
-
-        # update average_x1
-        average_x1 -= adjust_average_x(errors[random_index], w1, w2, y1, y2, average_x1, standart_deviation_x1, x, 1)
-
-        # update average_x2
-        average_x2 -= adjust_average_x(errors[random_index], w1, w2, y1, y2, average_x1, standart_deviation_x1, x, 2)
+        # find current average_error
+        average_error = 0
+        for e in errors:
+            average_error += e
+        average_error /= len(errors)
         
-        # update standart_deviation_x1
-        # remember: must use old average value
-        standart_deviation_x1 -= adjust_standart_deviation_x(errors[random_index], w1, w2, y1, y2, leg_average_x1, standart_deviation_x1, x, 1)
-       
-        # update standart_deviation_x2
-        # remember: must use old average value
-        standart_deviation_x2 -= adjust_standart_deviation_x(errors[random_index], w1, w2, y1, y2, leg_average_x2, standart_deviation_x2, x, 2)
+        # plot progress
+        if i % 200 == 0:
+            plot(xs, ys, i, average_error)
 
-    # find current average_error
-    average_error = 0
-    for e in errors:
-        average_error += e
-    average_error /= len(errors)
-    
-    # plot progress
-
-# plot final answer
+    # plot final answer
+    plot(xs, ys, ITERATION_LIMIT, average_error)
