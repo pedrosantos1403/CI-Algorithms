db40ac4f557eb05db2f5456682111b226d5b1d01
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-08-19 14:30:15 -0300

    set algorithm essencials

--- Fuzzy TSK/tsk.py
+++ Fuzzy TSK/tsk.py
@@ -1,29 +1,56 @@
 import math
 
 # define x universe
-begin = -20
-end = 20
-step = 1
-xs = [x for x in range(begin,end,step)]
+ITERATION_LIMIT = 1000
+BEGIN = -200
+END = 200
+LEARNING_RATE = 0.01
+STEP = 1
 
-# define average_x
-average_x = 0
-for x in xs:
-    average_x += x
-average_x /= len(xs)
+# define start average_x
+average_x1 = 0
+average_x2 = 0
 
-# define standart_deviation
-standart_deviation = 0
-for x in xs:
-    standart_deviation += (x - average_x)**2
-standart_deviation /= len(xs)
-standart_deviation = math.sqrt(standart_deviation)
+# define start standart_devitation_x
+standart_deviation_x1 = 0
+standart_deviation_x2 = 0
 
-# gaussiana?
-w = []
-for x in xs:
-    w.append(math.exp((-1/2)*((x-average_x)/standart_deviation)**2))
+# start xs and ys
+xs = [x for x in range(BEGIN, END, STEP)]
+ys = [0 for y in range(BEGIN, END, STEP)]
 
-# parameter p1?
+# start average_error
+average_error = 0
 
-# parameter p2?
+for i in range(ITERATION_LIMIT):
+    while(True):
+        # pass through xs in a random order
+        # break when there's no more index
+        random_index = 0
+        x = xs[random_index]
+
+        # calculate gaussian_w1 
+        # calculate gaussian_w2
+
+        # calculate y1 = p1 * x + q1
+        # calculate y2 = p2 * x + q2
+
+        # calculate ys[random_index] = (w1*y1 + w2*y2) / (w1 + w2)
+
+        # calculate errors[random_index] = ?
+
+        # update p1 ?
+        # update p2 ?
+        # update q1 ?
+        # update q2 ?
+
+        # update average_x1
+        # update average_x2
+        # update standart_deviation_x1
+        # update standart_deviation_x2
+
+        # update average_error
+    
+    # plot progress
+
+# plot final answer
