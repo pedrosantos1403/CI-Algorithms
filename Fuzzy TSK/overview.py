e9f002f9dac3896eccd6418fef9b65579430d29e
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-08-21 08:58:19 -0300

    add specs and overview

---
+++ Fuzzy TSK/overview.py
@@ -0,0 +1,21 @@
+# definir universo
+# estabecer taxa aprendizado
+# iniciar as referencias
+# criar os vetores pra x, y e erros
+# começar o loop
+    # percorrer todos os x's de forma aleatória e sem repetir
+        # calcular y1 e y2
+        # y1 = p1 * x + q1
+        # calcular w1 e w2
+        # calcular o y respectivo a media ponderada de w1y1 y2w2
+        # encontrar o erro da posição y calculado - x^2
+        # update p1 = p1 - taxa*(y-yd)(w1)/(w1+w2)x
+        # update p2
+        # update q1
+        # update q2
+        # update media_x1
+        # update media_x2
+        # update desvio_x1
+        # update desvio_x2
+    # plots de progresso
+# plot final
